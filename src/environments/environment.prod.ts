95044156bd0b783533c2f3363e4a2e2026bdb442
0gis0 <giselatb@outlook.com>
2021-10-22 12:53:30 +0200

    changed the URL for the API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   containerName: "alteregos",
-  apiUrl: "https://tour-of-heroes-webapi-background-process.azurewebsites.net/api/hero",
+  apiUrl: "https://tour-of-heroes-web-api.azurewebsites.net/api/hero",
   appInsights: {
     instrumentationKey: "66f63e51-580a-44b7-845b-4ddb1944ec0f"
   }

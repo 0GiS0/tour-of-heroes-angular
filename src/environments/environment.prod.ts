0a3d7e2297cb66f22dfc0983936c74c5e018cb78
0gis0 <giselatb@outlook.com>
2021-10-25 17:29:00 +0200

    added event hub to environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
   containerName: "alteregos",
-  apiUrl: "https://tour-of-heroes-web-api.azurewebsites.net/api/hero"  
+  apiUrl: "https://tour-of-heroes-web-api.azurewebsites.net/api/hero",
+  eventHub: {
+    endpoint: "https://direct-events.servicebus.windows.net",
+    hubName: "custom-events",
+  }
 };

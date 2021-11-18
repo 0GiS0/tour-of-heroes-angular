f4bde652a68942866022f816ccdf5bc0a3ecbf5f
0gis0 <giselatb@outlook.com>
2021-11-18 15:54:00 +0100

    change the order of the variablea

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: "https://tour-of-heroes-api-azure-storage.azurewebsites.net/api/hero",
-  production: true
+  production: true,
+  apiUrl: "https://tour-of-heroes-api-azure-storage.azurewebsites.net/api/hero"
 };

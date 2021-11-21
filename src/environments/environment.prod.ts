3917093a170c4ee9e66b9fd802457cf62e07e310
0gis0 <giselatb@outlook.com>
2021-11-21 16:53:10 +0100

    add storageUrl for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   appInsights: {
     instrumentationKey: "66f63e51-580a-44b7-845b-4ddb1944ec0f"
   },
+  storageUrl: "https://lemonheroes.blob.core.windows.net",
   signalRUrl: "https://signalrmessagingtourofheroes.azurewebsites.net/messaging"
 };

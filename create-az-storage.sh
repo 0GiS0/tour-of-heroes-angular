c6f1d5f20890cb28afd73e5df8cfb9562b460a3a
0gis0 <giselatb@outlook.com>
2021-11-21 11:20:01 +0100

    update steps with Azurite

--- create-az-storage.sh
+++ create-az-storage.sh
@@ -10,7 +10,7 @@ STORAGE_NAME="<YOUR_STORAGE_ACCOUNT_NAME>"
 LOCATION="northeurope"
 
 #Crear una cuenta de almacenamiento dentro del grupo de recursos Tour-Of-Heroes
-az storage account create --resource-group $RESOURCE_GROUP --name $STORAGE_NAME --location $LOCATION 
+az storage account create --resource-group $RESOURCE_GROUP --name $STORAGE_NAME --location $LOCATION
 
 #Crear el contenedor de los superheroes
 az storage container create --name heroes --account-name $STORAGE_NAME --public-access blob
@@ -22,3 +22,19 @@ az storage container create --name alteregos --account-name $STORAGE_NAME
 az storage blob upload-batch --destination alteregos --source src/assets/alteregos/. --account-name $STORAGE_NAME
 
 #Los héroes los subimos con Azure Storage Explorer
+
+#Using Azurite
+#Azurite is a free, open source, cross-platform, cloud-based development environment for building, testing, and deploying applications.
+npm install -g azurite
+
+#Connection string for azurite
+$AZURITE_CONNECTION_STRING="DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
+
+#Create containers
+az storage container create -n heroes --public-access blob --connection-string $AZURITE_CONNECTION_STRING
+az storage blob upload-batch --destination heroes --source src/assets/heroes/. --connection-string $AZURITE_CONNECTION_STRING
+
+az storage container create -n alteregos --connection-string $AZURITE_CONNECTION_STRING
+az storage blob upload-batch --destination alteregos --source src/assets/alteregos/. --connection-string $AZURITE_CONNECTION_STRING
+
+

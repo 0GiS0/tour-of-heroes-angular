eff4ceed4c22202a62d780340dca0009374cfcdb
0gis0 <giselatb@outlook.com>
2021-11-22 15:41:35 +0100

    delete extra slash from URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   containerName: "alteregos",
   apiUrl: "https://tour-of-heroes-free-account.azurewebsites.net/api/hero",
-  storageUrl: "https://lemonheroes.blob.core.windows.net/"
+  storageUrl: "https://lemonheroes.blob.core.windows.net"
 };

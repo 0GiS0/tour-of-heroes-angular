21adeaaef068d7a956cd34981ef271f77d3e65c0
0gis0 <giselatb@outlook.com>
2021-11-25 21:04:42 +0100

    update instrumentation key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   containerName: "alteregos",
   apiUrl: "https://tour-of-heroes-free-account.azurewebsites.net/api/hero",
   appInsights: {
-    instrumentationKey: "<INSTRUMENTATION_KEY>"
+    instrumentationKey: "e69ae07b-5bce-42f9-90f7-37d1e34f8c90"
   },
   storageUrl: "https://lemonheroes.blob.core.windows.net"
 };

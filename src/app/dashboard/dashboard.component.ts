b74c63e158118069fb3b3ad56295a2d4ad50c2ea
Gisela <giselatb@outlook.com>
2021-11-30 20:28:08 +0100

    fix dashboard to work locally

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -10,6 +11,7 @@ import { HeroService } from '../hero.service';
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[] = [];
+  storageUrl = environment.storageUrl;
 
   constructor(private heroService: HeroService) { }
 

05113b0a5b808554f5bfbc2991ca2c917d6d2d33
0gis0 <giselatb@outlook.com>
2021-12-01 18:48:02 +0100

    updated environment for App Insights

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   containerName: "alteregos",
   apiUrl: "https://localhost:5001/api/hero",
   appInsights: {
-    instrumentationKey: "<INSTRUMENTATION_KEY>"
+    instrumentationKey: "e69ae07b-5bce-42f9-90f7-37d1e34f8c90"
   },
   storageUrl: "http://127.0.0.1:10000/devstoreaccount1",
   signalRUrl: "https://localhost:7238/messaging"

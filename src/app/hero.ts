bc47abac7affed6afb322d5ab3c88e10311cb82d
Gisela <giselatb@outlook.com>
2022-04-22 09:41:29 +0200

    fix Name

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,4 @@
 export interface Hero {
     id: number;
-    name: string;
+    Name: string;
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,10 +21,10 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
-  add(name: string): void {
-    name = name.trim();
-    if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
+  add(Name: string): void {
+    Name = Name.trim();
+    if (!Name) { return; }
+    this.heroService.addHero({ Name } as Hero)
       .subscribe(hero => {
         this.heroes.push(hero);
       });

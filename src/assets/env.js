52a6bf4053622496760eac210009516bdecea6fc
0GiS0 <giselatb@outlook.com>
2023-03-10 12:36:49 +0100

    Update node modules

---
+++ src/assets/env.js
@@ -0,0 +1,6 @@
+(function (window) {
+    window['env'] = window['env'] || {};
+
+    // Environment variables
+    window['env']['ApiUrl'] = 'http://localhost:5010/api/hero';
+})(this);
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: window['env']['ApiUrl'] || 'http://localhost:5010/api/hero'
+};

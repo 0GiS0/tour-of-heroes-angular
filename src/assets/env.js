ec0065e93acdb2d058edb1ab76c98fea10cd5447
0GiS0 <giselatb@outlook.com>
2023-07-30 14:49:13 +0200

    update API url

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,5 +2,5 @@
     window['env'] = window['env'] || {};
 
     // Environment variables
-    window['env']['ApiUrl'] = 'http://localhost:5010/api/hero';
+    window['env']['ApiUrl'] = 'http://localhost:5000/api/hero';
 })(this);

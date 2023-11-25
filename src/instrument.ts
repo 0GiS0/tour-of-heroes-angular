391953b6666d556ba0da31844b4752dc574b8760
0GiS0 <giselatb@outlook.com>
2023-11-26 00:03:03 +0100

    Update devcontainer configuration and add Prometheus and Jaeger support

--- src/instrument.ts
+++ src/instrument.ts
@@ -7,22 +7,23 @@ import {
 } from '@opentelemetry/sdk-trace-web';
 import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-// import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+
 
 const provider = new WebTracerProvider();
-// For demo purposes only, immediately log traces to the console
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-// Batch traces before sending them to HoneyComb
+//provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+// Batch traces before sending them to Jaeger
 provider.addSpanProcessor(
   new BatchSpanProcessor(
     new OTLPTraceExporter({
-      url: 'http://simplest-collector.observability.svc:4317',
+      headers: {},
+      url: 'http://localhost:4318/v1/traces',
     }),
   ),
 );
 
 provider.register({
-  // contextManager: new ZoneContextManager(),
+  contextManager: new ZoneContextManager(),  
 });
 
 registerInstrumentations({
@@ -34,5 +35,5 @@ registerInstrumentations({
       '@opentelemetry/instrumentation-fetch': {},
       '@opentelemetry/instrumentation-xml-http-request': {},
     }),
-  ],
+  ],  
 });

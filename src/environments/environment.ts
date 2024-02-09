22648d9e63951448a978bf2566e59b114e65047e
0GiS0 <giselatb@outlook.com>
2024-02-09 21:06:35 +0100

    Update .gitignore and angular.json, and package.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/hero',
-  storageUrl: "http://127.0.0.1:10000/devstoreaccount1"
+  storageUrl: "http://localhost:10000/devstoreaccount1"
 };
 
 /*

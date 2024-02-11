af355e07dfc4ecd4afdc8abe3b93425d4af62499
Gisela Torres <giselatb@outlook.com>
2024-02-11 09:48:42 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:30040/api/hero'
-  apiUrl: window['env']['ApiUrl'] || 'http://localhost:5010/api/hero',
+  apiUrl: window['env']['ApiUrl'] || 'https://tour-of-heroes-api.azurewebsites.net/api/hero',
 };

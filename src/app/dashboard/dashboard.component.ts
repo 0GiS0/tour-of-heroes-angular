cb8f1bbf56ad3afd3103728645f2dd34bf32c0c0
0GiS0 <giselatb@outlook.com>
2024-02-11 23:14:28 +0100

    Update image URLs in dashboard and heroes components

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +10,7 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+  storageUrl = environment.storageUrl;
 
   constructor(private heroService: HeroService) {}
 

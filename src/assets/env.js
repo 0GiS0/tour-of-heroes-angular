389fdb0c3a0f2050ffa47ff33e454514e7b86f2d
Gisela Torres <giselatb@outlook.com>
2024-02-11 22:36:52 +0100

    Update env.js

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,5 +2,5 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["ApiUrl"] = "http://localhost:5000/api/hero";
+  window["env"]["ApiUrl"] = "https://tour-of-heroes-api.azurewebsites.net/api/hero";
 })(this);

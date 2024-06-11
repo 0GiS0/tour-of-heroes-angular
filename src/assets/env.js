84392442b5ed6c804c696806bd42695427dff801
Gisela Torres <giselatb@outlook.com>
2024-06-11 05:46:17 +0000

    chore: Update API URL in env.js file

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,5 +2,5 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["ApiUrl"] = "https://localhost:5001/api/hero";
+  window["env"]["ApiUrl"] = "https://refactored-space-memory-jrvqq9v7pc5v6j-5010.app.github.dev/api/hero";
 })(this);

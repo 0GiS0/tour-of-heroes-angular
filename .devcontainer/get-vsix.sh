00a903eb370c3347f3b6f71961c311f8ca8533e4
Gisela Torres <giselatb@outlook.com>
2024-06-11 23:13:42 +0000

    chore: Include GitHub Copilot Chat extension in devcontainer configuration

--- .devcontainer/get-vsix.sh
+++ .devcontainer/get-vsix.sh
@@ -12,4 +12,4 @@ curl ${curl_custom_flags} \
      -L \
      -H "Accept: application/octet-stream" \
      -H "Authorization: Bearer ${PAT_FOR_VSIX}" \
-        "https://api.github.com/repos/${OWNER_FOR_VSIX}/${REPO_FOR_VSIX}/releases/assets/${ASSET_ID}" -o "/tmp/vscode-azure-github-copilot.vsix""
+        "https://api.github.com/repos/${OWNER_FOR_VSIX}/${REPO_FOR_VSIX}/releases/assets/${ASSET_ID}" -o "/tmp/vscode-azure-github-copilot.vsix"

56d7ed48f3a49c4f4795bbb230f52c9e44557209
Gisela Torres <giselatb@outlook.com>
2024-06-18 12:06:57 +0000

    chore: Update API URL in env.js
    This commit updates the API URL in the `env.js` file to use the new endpoint "https://supreme-rotary-phone-6vq44wjrw244w4-5010.app.github.dev/api/hero". This change ensures that the application is making requests to the correct API endpoint.

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,6 +2,5 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["ApiUrl"] =
-    "https://improved-carnival-96rjjwv7ph7w4-5010.app.github.dev/api/hero";
+  window["env"]["ApiUrl"] = "https://supreme-rotary-phone-6vq44wjrw244w4-5010.app.github.dev/api/hero";
 })(this);

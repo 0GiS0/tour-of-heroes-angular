d7ba8208905de33a51657d68e32fd2cc138aa8d0
0GiS0 <giselatb@outlook.com>
2024-07-25 09:29:03 +0200

    chore: Update API URL to use localhost for development

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,6 +2,5 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["ApiUrl"] =
-    "https://stunning-space-funicular-qvjpp6579cx6jv-5010.app.github.dev/api/hero";
+  window["env"]["ApiUrl"] = "http://localhost:5010/api/hero";
 })(this);

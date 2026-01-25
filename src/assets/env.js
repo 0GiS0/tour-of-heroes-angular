a9e6376a6f9a72ea7bce6f8f8be9396bc7258470
Gisela Torres <giselatb@outlook.com>
2026-01-25 08:35:54 +0000

    chore: update devcontainer configuration and environment variables

--- src/assets/env.js
+++ src/assets/env.js
@@ -2,6 +2,5 @@
   window["env"] = window["env"] || {};
 
   // Environment variables
-  window["env"]["ApiUrl"] =
-    "https://stunning-space-funicular-qvjpp6579cx6jv-5010.app.github.dev/api/hero";
+  window["env"]["ApiUrl"] = "https://-5030./api/hero";
 })(this);
